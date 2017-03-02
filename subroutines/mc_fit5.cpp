d7bde22bd7d9de24cfd9e376d1bd11e62398161b
dhollan9 <davido.holland@gmail.com>
2017-03-02 10:55:37 -0500

    Update mc_fit5.cpp

--- subroutines/mc_fit5.cpp
+++ subroutines/mc_fit5.cpp
@@ -15,7 +15,7 @@ double mc_fit5(int ninterface, double *grid_cofs, double *clocals, double ksi, d
 
 
     double tmp;
-    cout<<"ksi component of fitness is: " << fitness << endl;
+    //cout<<"ksi component of fitness is: " << fitness << endl;
     tmp = fitness;
 
     for(i=0;i<ninterface;i++){
@@ -23,16 +23,16 @@ double mc_fit5(int ninterface, double *grid_cofs, double *clocals, double ksi, d
 	fitness+=exp(beta*clocals[i])-1; //clocal>0 means a triangle present. Penalize this.
       }
     }
-    cout << "The beta component is: " << fitness-tmp << endl;
+    //cout << "The beta component is: " << fitness-tmp << endl;
     tmp=fitness;
     //This version penalizes the number of extra interfaces, not interfaces per protein.
     
     fitness+=exp(mu*(ninterface-nwhole));
 
-    cout << "The mu component is: " << fitness-tmp << endl;
+    //cout << "The mu component is: " << fitness-tmp << endl;
     tmp=fitness;
     fitness+=exp(edgediff); //Penalizes having too many edges
-    cout<<"The omega component is: " << fitness-tmp << endl;
+    //cout<<"The omega component is: " << fitness-tmp << endl;
     return fitness;
     
 }
